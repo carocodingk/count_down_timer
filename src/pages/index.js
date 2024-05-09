06bdc2d3a7114b7079009de2a548ee2c4372ca9b
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2024-05-09 01:47:30 -0400

    Fixed bug that skipped a second when timer starts and added a sign to indicate when time is up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,101 @@
 import { useEffect, useState } from "react";
 
+const defaultTotalTime = 3600;
 
-export default function Home() {
-  const pomodoroTime = 20 * 10;
-  const [start, setStart] = useState(false);
-  const [resetVal, setResetVal] = useState(false);
-  const [seconds, setSeconds] = useState(pomodoroTime);
-
-  const secondsConvert = (totalTime) => {
-    const minutes = Math.floor(totalTime / 60)
-    const seconds = totalTime % 60
-    console.log(minutes, seconds)
-    return[minutes, seconds]
-  }
-
-  
-
-  console.log(secondsConvert(pomodoroTime))
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (start ){
-        setSeconds((seconds) => seconds - 1)
-        if (seconds === 0){
-          setStart(false)
-          console.log('ALARM')
-          setSeconds(pomodoroTime)
-        }
-      }
-      else if (!start && resetVal){
-        setSeconds(pomodoroTime)
-        setResetVal(false)
-      }
-      }, 1000)
-  }, [start, resetVal, seconds]);
- 
-  return (
-    <div>
-      <div>{seconds}</div>
-      <div>
-        <input type="button" value='START' onClick={() => setStart(!start)} />
-        <input type="button" value='RESET' onClick={() => {setStart(false); setResetVal(true)}} />
-      </div>
+function Alarm ({resetAlarm}){
+  return(
+    <div className="timesUp1">
+      <h1>TIME IS UP!</h1>
+      <input type="button" value="OK" className="button" onClick={() => resetAlarm()} />
     </div>
   );
 }
+
+
+export default function Home() {
+  const [start, setStart] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [totalTime, setTotalTime] = useState(defaultTotalTime);
+  const [timesUp, setTimesUp] = useState(false);
+  const [editedMinutes, setEditedMinutes] = useState(0);
+  const [editedSeconds, setEditedSeconds] = useState(0);
+
+  useEffect(() => {
+    let timer;
+    if (start) {
+      timer = setTimeout(() => {
+        setTotalTime((prevTotalTime) => prevTotalTime - 1);
+      }, 1000);
+      if (totalTime === 0){
+        setStart(false)
+        console.log('alarm')
+        setTimesUp(true)
+      }
+    }
+    return () => {
+      if (timer) {
+        clearTimeout(timer)
+      }
+    };
+  }, [start, totalTime]);
+
+  const seconds = totalTime % 60;
+  const minutes = Math.floor(totalTime / 60);
+
+
+  const customizedTime = () => {
+    if (edit){
+      setTotalTime(Number(editedMinutes * 60) + Number(editedSeconds))
+      setEditedMinutes(0)
+      setEditedSeconds(0)
+    }
+    setStart(false)
+    setEdit(!edit); 
+    setTimesUp(false)
+  }
+
+  const resetAlarm = () => {
+    console.log('click'); 
+    setTimesUp(false);
+    setTotalTime(defaultTotalTime)
+  }
+
+  return (
+    <div>
+      <div id="timer">
+        {
+        edit?
+        <div className="flexbox timebox">
+          <div className="flexbox colflex">
+            <input type="number" name="newMinutes" className="timeInput" min={0} max={60} onChange={(e) => setEditedMinutes(e.target.value)} />
+            <p>Minutes</p>
+          </div>
+          <div id="colon">
+            <h1>:</h1>
+          </div>
+          <div className="flexbox colflex">
+            <input type="number" name="newSeconds" className="timeInput" min={0} max={60} onChange={(e) => setEditedSeconds(e.target.value)} />
+            <p>Seconds</p>
+          </div>
+        </div>
+        :
+        <div id="timeText" className="timebox">{minutes}:{seconds < 10? `0${seconds}`: seconds}</div>
+        }
+        <div>
+          <input className="button" type="button" value={start? 'PAUSE' : 'START'} onClick={() => setStart(!start)} />
+          <input className="button" type="button" value={edit? 'OK' : 'EDIT'} onClick={() => {customizedTime()}} />
+          <input className="button" type="button" value='RESET' onClick={() => {
+            setStart(false);
+            setTotalTime(defaultTotalTime);
+            setTimesUp(false)
+          }} />
+        </div>
+      </div>
+      <div className="timesUp">
+        {
+        timesUp && <Alarm resetAlarm={resetAlarm} />
+        }
+      </div>
+    </div>
+  );
+};
